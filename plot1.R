60872e1205bd2d8b06d7c5ed1dc8d21b01c2936b
elizabeth gillbrand <elizabeth@gillbrand.com>
2014-10-12 14:09:58 -0700

    Submitting project 1 plot 1

---
+++ plot1.R
@@ -0,0 +1,12 @@
+url<-https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+download.file(url, destfile = "./household_power_consumption.zip", method = "curl")  
+unzip("./household_power_consumption.zip")
+p_data<-read.table("household_power_consumption.txt", sep = ";", header = TRUE)
+DT<-paste(p_data$Date,p_data$Time, sep = "")
+DTpos<-strptime(DT, format = "%d/%m/%Y %H:%M:%S")
+p_data<-cbind(p_data,DTpos)
+index<-DTpos$year==107&DTpos$mon==1&DTpos$mday==1|DTpos$year==107&DTpos$mon==1&DTpos$mday==2
+data<-p_data[index,]
+hist((as.numeric(data$Global_active_power)/1000), col = "red", main = "Global Active Power", xlab= "Global Active Power(kilowats)")
+dev.copy(png, file = "~/plot1.png")
+dev.off()
